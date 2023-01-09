069739e52b8afa014c8dbf5c3c886ce688691e9d
stubbers1 <o.stubley@gmail.com>
2023-01-09 20:06:30 +0000

    failed attempt of menu

--- sketch.js
+++ sketch.js
@@ -1,13 +1,31 @@
 let player;
+let inGame = False;
+let inMenu = True;
+
+
+
+function mousePressed(){
+  if (inMenu = True){
+    inGame = doesGameStart();
+    if (inGame = True){
+      inMenu = False;
+    }
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   player = new Player();
+  menu = new startMenu();
 }
 
 function draw() {
   background(220);
-
-  player.update();
+  if (inGame = True){
+    player.update();
+  }else if (inMenu = True){
+   menu.display();
+   
+ }
 }
---
+++ src/menu/startMenu.js
@@ -0,0 +1,15 @@
+class startMenu{
+
+    constructor(){
+    }
+
+    display(){
+        textSize(50);
+        text("Catch Game!",10, 100);
+    }
+    
+    doesGameStart(){
+
+    }
+
+}
