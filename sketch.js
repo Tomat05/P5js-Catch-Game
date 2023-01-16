0081607c0b9aa94ee751dea9d687597fc38ad50a
Tomat05 <64409983+Tomat05@users.noreply.github.com>
2023-01-16 13:26:41 +0000

    Menu (#2)
    * failed attempt of menu
    
    * fixed bugs in menu
    
    * Button still isnt buttoning
    
    Co-authored-by: stubbers1 <o.stubley@gmail.com>

--- sketch.js
+++ sketch.js
@@ -1,20 +1,34 @@
 let player;
-let spawner;
-
-let millisOld = 0;
+let inGame = false;
+let inMenu = true;
+let menu;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+  
+  menu = new startMenu();
 
 	player = new Player();
-
-	spawner = new ObjectSpawner();
+  
+  spawner = new ObjectSpawner();
 	spawner.setup();
 }
 
 function draw() {
-	background(0);
+  background(0);
+  if (inGame){
+    player.onFrameUpdate();
+    spawner.onFrameUpdate();
+  }else if (inMenu){
+    menu.display();
+  }
+}
 
-	player.onFrameUpdate();
-	spawner.onFrameUpdate();
+function mousePressed(){
+	if (inMenu){
+	  inGame = menu.doesGameStart();
+	  if (inGame){
+		inMenu = false;
+	  }
+	}
 }
