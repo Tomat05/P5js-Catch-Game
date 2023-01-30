23ef8d105f68d8c5fb93f359c5199fcb554c9f9c
Tomat05 <64409983+Tomat05@users.noreply.github.com>
2023-01-30 14:01:28 +0000

    Virussy so good, I messed the thing up

--- src/gameplay/ObjectSpawner.js
+++ src/gameplay/ObjectSpawner.js
@@ -24,16 +24,11 @@ class ObjectSpawner {
 
 	spawn() {
 		let obj = this.fallingObjectsPool.shift();
-<<<<<<< HEAD
-		let goodOrBad = Math.round(Math.random());
-		obj.setActive(goodOrBad, this.images[goodOrBad]);
-=======
 		let goodOrBad = 0
 		if (Math.round(Math.random()) === 0) {
 			goodOrBad = Math.round(Math.random());
 		}
-		obj.setActive(goodOrBad);
->>>>>>> in-progress
+		obj.setActive(goodOrBad, this.images[goodOrBad]);
 		this.activeObjects.push(obj);
 	}
 
